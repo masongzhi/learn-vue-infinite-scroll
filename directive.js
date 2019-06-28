b5ba7345a543f436ec1554e9370fc3e5d15149d2
MASONGZHI <511071161@qq.com>
2019-06-28 15:47:30 +0800

    Create directive.js

---
+++ directive.js
@@ -0,0 +1,225 @@
+const ctx = '@@InfiniteScroll';
+
+var throttle = function (fn, delay) {
+  var now, lastExec, timer, context, args; //eslint-disable-line
+
+  var execute = function () {
+    fn.apply(context, args);
+    lastExec = now;
+  };
+
+  return function () {
+    context = this;
+    args = arguments;
+
+    now = Date.now();
+
+    if (timer) {
+      clearTimeout(timer);
+      timer = null;
+    }
+
+    if (lastExec) {
+      var diff = delay - (now - lastExec);
+      if (diff < 0) {
+        execute();
+      } else {
+        timer = setTimeout(() => {
+          execute();
+        }, diff);
+      }
+    } else {
+      execute();
+    }
+  };
+};
+
+var getScrollTop = function (element) {
+  if (element === window) {
+    return Math.max(window.pageYOffset || 0, document.documentElement.scrollTop);
+  }
+
+  return element.scrollTop;
+};
+
+var getComputedStyle = document.defaultView.getComputedStyle;
+
+// 获取滚动主体，1：element设置了高度，滚动主体为element 2：是element高度超过外层限定高度，滚动主体为外层或外层的外层
+var getScrollEventTarget = function (element) {
+  var currentNode = element;
+  // bugfix, see http://w3help.org/zh-cn/causes/SD9013 and http://stackoverflow.com/questions/17016740/onscroll-function-is-not-working-for-chrome
+  while (currentNode && currentNode.tagName !== 'HTML' && currentNode.tagName !== 'BODY' && currentNode.nodeType === 1) {
+    var overflowY = getComputedStyle(currentNode).overflowY;
+    if (overflowY === 'scroll' || overflowY === 'auto') {
+      return currentNode;
+    }
+    currentNode = currentNode.parentNode;
+  }
+  return window;
+};
+
+var getVisibleHeight = function (element) {
+  if (element === window) {
+    return document.documentElement.clientHeight;
+  }
+
+  return element.clientHeight;
+};
+
+var getElementTop = function (element) {
+  if (element === window) {
+    return getScrollTop(window);
+  }
+  return element.getBoundingClientRect().top + getScrollTop(window);
+};
+
+var isAttached = function (element) {
+  var currentNode = element.parentNode;
+  while (currentNode) {
+    if (currentNode.tagName === 'HTML') {
+      return true;
+    }
+    if (currentNode.nodeType === 11) {
+      return false;
+    }
+    currentNode = currentNode.parentNode;
+  }
+  return false;
+};
+
+var doBind = function () {
+  if (this.binded) return; // eslint-disable-line
+  this.binded = true;
+
+  var directive = this;
+  var element = directive.el;
+
+  // 获取节流值
+  var throttleDelayExpr = element.getAttribute('infinite-scroll-throttle-delay');
+  var throttleDelay = 200;
+  if (throttleDelayExpr) {
+    throttleDelay = Number(directive.vm[throttleDelayExpr] || throttleDelayExpr);
+    if (isNaN(throttleDelay) || throttleDelay < 0) {
+      throttleDelay = 200;
+    }
+  }
+  directive.throttleDelay = throttleDelay;
+  // 获取节流值end
+
+  directive.scrollEventTarget = getScrollEventTarget(element);
+  directive.scrollListener = throttle(doCheck.bind(directive), directive.throttleDelay);
+  directive.scrollEventTarget.addEventListener('scroll', directive.scrollListener);
+
+  // 注销绑定事件
+  this.vm.$on('hook:beforeDestroy', function () {
+    directive.scrollEventTarget.removeEventListener('scroll', directive.scrollListener);
+  });
+
+  // 获取是否禁止触发滚动加载
+  var disabledExpr = element.getAttribute('infinite-scroll-disabled');
+  var disabled = false;
+
+  if (disabledExpr) {
+    this.vm.$watch(disabledExpr, function(value) {
+      directive.disabled = value;
+      if (!value && directive.immediateCheck) {
+        doCheck.call(directive);
+      }
+    });
+    disabled = Boolean(directive.vm[disabledExpr]);
+  }
+  directive.disabled = disabled;
+  // 获取是否禁止触发滚动加载end
+
+  // 获取距离底部高度
+  var distanceExpr = element.getAttribute('infinite-scroll-distance');
+  var distance = 0;
+  if (distanceExpr) {
+    distance = Number(directive.vm[distanceExpr] || distanceExpr);
+    if (isNaN(distance)) {
+      distance = 0;
+    }
+  }
+  directive.distance = distance;
+  // 获取距离底部高度end
+
+  var immediateCheckExpr = element.getAttribute('infinite-scroll-immediate-check');
+  var immediateCheck = true;
+  if (immediateCheckExpr) {
+    immediateCheck = Boolean(directive.vm[immediateCheckExpr]);
+  }
+  directive.immediateCheck = immediateCheck;
+
+  if (immediateCheck) {
+    doCheck.call(directive);
+  }
+
+  var eventName = element.getAttribute('infinite-scroll-listen-for-event');
+  if (eventName) {
+    directive.vm.$on(eventName, function() {
+      doCheck.call(directive);
+    });
+  }
+};
+
+var doCheck = function (force) {
+  var scrollEventTarget = this.scrollEventTarget;
+  var element = this.el;
+  var distance = this.distance;
+
+  if (force !== true && this.disabled) return; //eslint-disable-line
+  var viewportScrollTop = getScrollTop(scrollEventTarget);
+  var viewportBottom = viewportScrollTop + getVisibleHeight(scrollEventTarget);
+
+  var shouldTrigger = false;
+
+  if (scrollEventTarget === element) {
+    // 总高度 - 滚动高度 - 可视高度 <= distance
+    shouldTrigger = scrollEventTarget.scrollHeight - viewportBottom <= distance;
+  } else {
+    var elementBottom = getElementTop(element) - getElementTop(scrollEventTarget) + element.offsetHeight + viewportScrollTop;
+
+    shouldTrigger = viewportBottom + distance >= elementBottom;
+  }
+
+  if (shouldTrigger && this.expression) {
+    this.expression();
+  }
+};
+
+export default {
+  bind(el, binding, vnode) {
+    el[ctx] = {
+      el,
+      vm: vnode.context,
+      expression: binding.value
+    };
+    const args = arguments;
+    el[ctx].vm.$on('hook:mounted', function () {
+      el[ctx].vm.$nextTick(function () {
+        if (isAttached(el)) {
+          doBind.call(el[ctx], args);
+        }
+
+        el[ctx].bindTryCount = 0;
+
+        var tryBind = function () {
+          if (el[ctx].bindTryCount > 10) return; //eslint-disable-line
+          el[ctx].bindTryCount++;
+          if (isAttached(el)) {
+            doBind.call(el[ctx], args);
+          } else {
+            setTimeout(tryBind, 50);
+          }
+        };
+
+        tryBind();
+      });
+    });
+  },
+
+  unbind(el) {
+    if (el && el[ctx] && el[ctx].scrollEventTarget)
+      el[ctx].scrollEventTarget.removeEventListener('scroll', el[ctx].scrollListener);
+  }
+};
